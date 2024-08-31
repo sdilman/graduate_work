cfafe7f3b3d282fa00f899683542b976eba5f0d2
bakhee <alkesey.gredyaev@gmail.com>
2024-08-31 23:40:56 +0300

    refactor(auth): apply new service structure

--- services/auth/auth-service/src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.models.db_entity import User, Role, UserRole, Permission, RolePermission  # noqa
-from src.models.oauth import SocialNetworks
-
-
-__all__ = ["User", "Role", "UserRole", "Permission", "RolePermission", "SocialNetworks"]
