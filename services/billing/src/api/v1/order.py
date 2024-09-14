84fffbc389477c48b1b61c590b9bdc50e5613136
bakhee <alkesey.gredyaev@gmail.com>
2024-09-14 23:01:47 +0300

    refactor(billing): api get user_id from request (#30)

--- services/billing/src/api/v1/order.py
+++ services/billing/src/api/v1/order.py
@@ -2,13 +2,11 @@ from __future__ import annotations
 
 from typing import Annotated, Any
 
-from fastapi import APIRouter, Cookie, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.postgres import get_pg_session
-from schemas.cookie import AccessTokenCookie
 from schemas.entity import OrderSchema
-from services.authentication import AuthService, get_auth_service
 from services.entity import EntityService, get_entity_service
 
 router = APIRouter()
@@ -16,12 +14,10 @@ router = APIRouter()
 
 @router.post("/create_order")
 async def create_order(
-    auth_service: Annotated[AuthService, Depends(get_auth_service)],
+    request: Request,
     entity_service: Annotated[EntityService, Depends(get_entity_service)],
     db: Annotated[AsyncSession, Depends(get_pg_session)],
     input_order: OrderSchema,
-    input_token: str = Cookie(alias=AccessTokenCookie.name),
 ) -> Any:
-    user_external = await auth_service.authenticate_user(input_token)
-    # TODO: проверка ID юзера - должен совпасть sso_id
+    input_order.user_id = request.state.user_id
     return await entity_service.create_order(db, input_order)
