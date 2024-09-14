980b519666df0d2b9ae1bcda8d8ecdac87ddd0b9
bakhee <alkesey.gredyaev@gmail.com>
2024-09-14 19:16:29 +0300

    feat(billing): add PermissionsMiddleware (#30)

---
+++ services/billing/src/helpers/exempt_endpoints.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+from core.settings import settings as config
+
+
+class ExemptEndpoints(BaseModel):
+    """List of exempt endpoints."""
+
+    healthcheck: str = "/healthcheck"
+    docs: str = config.api.docs_url.split(config.api.version)[-1]
+    openai: str = config.api.openapi_url.split(config.api.version)[-1]
+
+
+def get_exempt_endpoints() -> list[str]:
+    """Get list of exempt endpoints"""
+    exempt_endpoints = ExemptEndpoints().model_dump().values()
+    return list(exempt_endpoints)
