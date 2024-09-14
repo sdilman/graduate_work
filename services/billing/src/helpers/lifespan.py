b8e88ac596152f2864d2e91b959a43d8906cdf64
bakhee <alkesey.gredyaev@gmail.com>
2024-09-14 19:10:54 +0300

    refactor(billing): lifespan

--- services/billing/src/helpers/lifespan.py
+++ services/billing/src/helpers/lifespan.py
@@ -1,23 +1,18 @@
 from typing import AsyncGenerator
 
-import logging
-
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from redis.asyncio import Redis
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:  # noqa: ARG001
-    from core.settings import settings
-    from db import redis
+async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
+    from broker import initialize_kafka_topics
+    from db.redis import redis_manager as redis
 
-    # On startup events
-    logging.info("Config: %s", vars(settings))
-    redis.redis = Redis(host=settings.redis.host, port=settings.redis.port)
-    logging.info("Redis: %s", str(redis))
-
-    yield
-    # On shutdown events
-    await redis.close()
+    try:
+        await redis.initialize()
+        await initialize_kafka_topics()
+        yield
+    finally:
+        await redis.close()
