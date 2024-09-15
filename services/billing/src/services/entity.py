79171c14ad7e5b63f2ae4a69cec55fb5e825529e
bakhee <alkesey.gredyaev@gmail.com>
2024-09-16 02:32:21 +0300

    refactor(billing) services product_service.py - move provider to init - move db from method arguments to init

--- services/billing/src/services/entity.py
+++ services/billing/src/services/entity.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from datetime import datetime
-from functools import lru_cache
 from uuid import UUID  # noqa: TCH003
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,32 +13,30 @@ logger = get_logger(__name__)
 
 
 class EntityService:
-    async def create_product(self, db: AsyncSession, product_schema: ProductSchema) -> ProductSchema:
-        new_product = Product(**product_schema.dict())
-        db.add(new_product)
-        await db.commit()
-        await db.refresh(new_product)
+    def __init__(self, db: AsyncSession):
+        self._db = db
+
+    async def create_product(self, product_schema: ProductSchema) -> ProductSchema:
+        new_product = Product(**product_schema.model_dump())
+        self._db.add(new_product)
+        await self._db.commit()
+        await self._db.refresh(new_product)
         return new_product.id
 
-    async def create_transaction(self, db: AsyncSession, transaction_schema: TransactionSchema) -> str:
+    async def create_transaction(self, transaction_schema: TransactionSchema) -> str:
         import logging
 
         logging.warning(transaction_schema.model_dump(exclude_unset=True))
         new_transaction = Transaction(**transaction_schema.model_dump(exclude_unset=True))
-        db.add(new_transaction)
-        await db.commit()
+        self._db.add(new_transaction)
+        await self._db.commit()
         return str(new_transaction.id)
 
-    async def create_user_product(self, db: AsyncSession, user_id: UUID, product_id: UUID) -> str:
+    async def create_user_product(self, user_id: UUID, product_id: UUID) -> str:
         new_user_product = UserProduct(
             product_id=product_id, user_id=user_id, active_from=datetime.now(), renewal_enabled=True
         )
-        db.add(new_user_product)
-        await db.commit()
-        await db.refresh(new_user_product)
+        self._db.add(new_user_product)
+        await self._db.commit()
+        await self._db.refresh(new_user_product)
         return str(new_user_product.id)
-
-
-@lru_cache
-def get_entity_service() -> EntityService:
-    return EntityService()
