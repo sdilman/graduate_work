e1285da9af8dc45dba36386beef53c80b033b73c
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2024-09-15 12:46:59 +0300

    change kafka message key and value types from str to bytes

--- services/billing/src/schemas/broker.py
+++ services/billing/src/schemas/broker.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 
 class MessageIn(BaseModel):
     topic: str = Field(..., description="Name of the Kafka topic")
-    key: str = Field(..., description="Key of the message with idempotency key")
-    value: str = Field(..., description="Value of the message")
+    key: bytes = Field(..., description="Key of the message with idempotency key")
+    value: bytes = Field(..., description="Value of the message")
