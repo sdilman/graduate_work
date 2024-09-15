882a3c92ac3e461b0c46c8380edc250437537cac
bakhee <alkesey.gredyaev@gmail.com>
2024-09-15 23:00:56 +0300

    rebase from main

--- services/billing/src/api/v1/order.py
+++ services/billing/src/api/v1/order.py
@@ -3,20 +3,17 @@ from __future__ import annotations
 from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends, Request
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.postgres import get_pg_session
 from schemas.entity import OrderSchema
-from services.order_service import get_order_service
+from services import OrderService, get_order_service
 
 router = APIRouter()
 
 
 @router.post("/create_order")
 async def create_order(
-    request: Request, db: Annotated[AsyncSession, Depends(get_pg_session)], input_order: OrderSchema
+    request: Request, schema_in: OrderSchema, service: Annotated[OrderService, Depends(get_order_service)]
 ) -> Any:
-    order_service = get_order_service(db)
-    input_order.user_id = request.state.user_id
-    order = await order_service.create_order(input_order)
+    schema_in.user_id = request.state.user_id
+    order = await service.create_order(schema_in)
     return order.id
