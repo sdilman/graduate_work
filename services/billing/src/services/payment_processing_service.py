4e6839381c553a5ec7eb71208ea340669fcfc7d1
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2024-09-15 15:39:15 +0300

    fix import of db enum

--- services/billing/src/services/payment_processing_service.py
+++ services/billing/src/services/payment_processing_service.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import select
 
+from core.constraints import OrderStatus
 from core.logger import get_logger
-from models.enums import OrderStatus
 from models.pg import Order, OrderProduct, PaymentMethod, Product, Transaction, UserProduct
 from schemas.youkassa import YoukassaPaymentObject, YoukassaPaymentStatuses
 
