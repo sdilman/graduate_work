c20be25e719075c59235d72a8917f25a070ecd62
bakhee <alkesey.gredyaev@gmail.com>
2024-09-16 01:09:21 +0300

    refactor(billing): api order.py - add documentation - add status code

--- services/billing/src/api/v1/order.py
+++ services/billing/src/api/v1/order.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends, Request
+from starlette import status
 
 from schemas.entity import OrderSchema
 from services import OrderService, get_order_service
@@ -10,7 +11,16 @@ from services import OrderService, get_order_service
 router = APIRouter()
 
 
-@router.post("/create_order")
+@router.post(
+    "/create_order",
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a new order",
+    description="""
+        The server validates the provided **idempotency key** by checking it against the cache to prevent duplicate operations.
+        If the key is already present in the cache, the request is rejected with a **409 Conflict** error, ensuring that the same
+        operation is not processed multiple times.
+    """,
+)
 async def create_order(
     request: Request, schema_in: OrderSchema, service: Annotated[OrderService, Depends(get_order_service)]
 ) -> Any:
